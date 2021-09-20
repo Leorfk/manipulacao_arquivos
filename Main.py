d2155ce138bf6f1b6411079b43d842ebd46dcddb
leorfk <leonardorodrigues_f@hotmail.com>
2021-09-19 23:20:20 -0300

    refatoraçao da aplicaçao

--- Main.py
+++ Main.py
@@ -7,16 +7,16 @@ import os
 def gerar_massa(file_utils, files_producer):
     for c in range(2):
         small_file = file_utils.generate_file(path, 'xap')
-        files_producer.write_events_to_file(small_file, generate_event, 50000)
+        files_producer.write_events_to_file(small_file, generate_event, 500)
 
 
 if __name__ == '__main__':
     path = os.getcwdb().decode('utf-8') + '/eventos/'
-
+    concat_file = os.getcwdb().decode('utf-8') + '/eventos_consolidados/'
     files_producer = SmallFilesProducer(path)
     file_utils = FileUtils()
-
     gerar_massa(file_utils, files_producer)
+    file_list = file_utils.get_files_in_dir(path)
 
-
-
+    big_file = file_utils.generate_file(concat_file, 'consolidado')
+    file_utils.concat_content_of_files(big_file, file_list, path)
--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,7 +1,32 @@
 from datetime import datetime
+import os
 
 
 class FileUtils:
 
     def generate_file(self, file_path, file_name):
+        if not os.path.exists(file_path):
+            os.mkdir(file_path)
         return open(f'{file_path}{file_name}_{str(datetime.now().timestamp())}.csv', 'x')
+
+    def get_files_in_dir(self, file_path):
+        list_of_files = []
+        for (dirpath, dirnames, filenames) in os.walk(file_path):
+            list_of_files.extend(filenames)
+            break
+        return list_of_files
+
+    def concat_content_of_files(self, big_file, file_names, file_path):
+        for fname in file_names:
+            with open(file_path+fname) as infile:
+                for line in infile:
+                    big_file.write(line)
+
+    def delete_files_in_dir(self, file_path, file_names):
+        print('hora do delete')
+        for file in file_names:
+            os.remove(file_path + file)
+        print(f'arquivos no direitório {file_path} foram deletados')
+
+    def delete_file_in_dir(self, file_path, file_name):
+        os.remove(file_path+file_name)
